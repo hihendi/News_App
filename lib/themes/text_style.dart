7770d4cffe59d85719b48f375eb74e1efe42f64e
Hendi Irwanto <hi.hendiirwanto@gmail.com>
2022-07-11 23:28:10 +0700

    add textstyle

---
+++ lib/themes/text_style.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//Display TextStyle
+
+TextStyle displayLarge = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    fontSize: 48,
+  ),
+);
+
+TextStyle displayMedium = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    fontSize: 32,
+  ),
+);
+
+TextStyle displaySmall = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    fontSize: 24,
+  ),
+);
+
+TextStyle displayLargeBold = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.bold,
+  ),
+);
+
+TextStyle displayMediumBold = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+  ),
+);
+
+TextStyle displaySmallBold = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  ),
+);
+
+//Text TextStyle
+
+TextStyle textLarge = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    fontSize: 20,
+  ),
+);
+TextStyle textMedium = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    fontSize: 16,
+  ),
+);
+TextStyle textSmall = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    fontSize: 14,
+  ),
+);
+TextStyle textXSmall = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    fontSize: 13,
+  ),
+);
