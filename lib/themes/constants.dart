f2e86490ac52410f46252dc87ac4db58a01f4b01
Hendi Irwanto <hi.hendiirwanto@gmail.com>
2022-07-14 08:07:24 +0700

    add file constant for margin and size screen

---
+++ lib/themes/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// Size screenSize(context) {
+//   return MediaQuery.of(context).size;
+// }
+
+class SizeConfig {
+  static MediaQueryData? _mediaQueryData;
+  static double? screenWidth;
+  static double? screenHeight;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData!.size.width;
+    screenHeight = _mediaQueryData!.size.height;
+  }
+}
+
+double defaultMargin = 24;
